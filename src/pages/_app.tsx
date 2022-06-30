0f31591e758c5f223505891c91fb9e8a5115af86
alkotras42 <kirill.stogniy@mail.ru>
2022-07-01 01:00:47 +0300

    Add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
+import superjson from 'superjson'
+import { AppRouter } from '../server/route/app.router'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc` : 'http://localhost:3000/api/trpc'
+    return {
+      headers() {
+        if(ctx?.req) {
+          return {
+            ...ctx.req.headers,
+            'x-ssr': '1'
+          }
+        }
+        return {} 
+      },
+      url,
+      transformer: superjson,
+    }
+  },
+  ssr: false
+})(MyApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {trpc} from '../utils/trpс'
 
 export default function Home() {
+
+  const {data, error, isLoading} = trpc.useQuery(['hello'])
+
+  if(isLoading) return <p>Loading...</p>
+
+  if(error) return <p>Something went wrong</p>
+
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      {JSON.stringify(data)}
     </h1>
   )
 }
